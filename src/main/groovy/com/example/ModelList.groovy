f01f4545101d099483421d1f4adaca6b10fe2023
Paul King <paulk@asert.com.au>
2023-08-28 20:50:40 +1000

    optionally hide 'object' from model list

--- src/main/groovy/com/example/ModelList.groovy
+++ src/main/groovy/com/example/ModelList.groovy
@@ -1,11 +1,12 @@
 package com.example
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import groovy.transform.ToString
 import io.micronaut.serde.annotation.Serdeable
 
 @Serdeable
 @ToString
+@JsonIgnoreProperties('object')
 class ModelList {
-    String object
     List<Model> data
 }
